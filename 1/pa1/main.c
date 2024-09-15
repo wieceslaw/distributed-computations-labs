c1a5abf73cc04f3c60c74be93e0c5e237b720b68
wieceslaw <vyache-nov@mail.ru>
2024-09-15 22:38:13 +0300

    upd

--- 1/pa1/main.c
+++ 1/pa1/main.c
@@ -3,6 +3,7 @@
 #include "ipc.h"
 #include "pa1.h"
 
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,7 +12,7 @@
 
 FILE *pipes_log_fd;
 FILE *event_log_fd;
-local_id current_id;
+local_id current_pid;
 
 typedef struct {
     int rfd;
@@ -27,11 +28,6 @@ typedef struct {
 typedef struct {
     int data[2];
 } pipe_desc;
-
-enum {
-    FIRST_CHILD_ID = 1,
-    EMPTY_DESCRIPTOR = -1
-};
 
 static int read_blocking(const int fd, char *buffer, const size_t size) {
     if (size == 0) {
@@ -76,12 +72,14 @@ int send(void *self, local_id dst, const Message *msg) {
     if (msg->s_header.s_magic != MESSAGE_MAGIC) {
         return -1;
     }
-
     Process *process = (Process *) self;
     if (process->id == dst) {
         return -1;
     }
-    if (dst < 0 || dst >= process->channels_size) {
+    if (process->id == PARENT_ID) {
+        return -1;
+    }
+    if (dst >= process->channels_size) {
         return -1;
     }
 
@@ -92,8 +90,11 @@ int send_multicast(void *self, const Message *msg) {
     if (msg->s_header.s_magic != MESSAGE_MAGIC) {
         return -1;
     }
-
     Process *process = (Process *) self;
+    if (process->id == PARENT_ID) {
+        return -1;
+    }
+
     for (local_id dst = 0; dst < process->channels_size; dst++) {
         if (process->id == dst) {
             continue;
@@ -108,7 +109,7 @@ int send_multicast(void *self, const Message *msg) {
 
 int receive(void *self, local_id from, Message *msg) {
     Process *process = (Process *) self;
-    if (from == process->id || from < 0 || from >= process->channels_size) {
+    if (from == process->id || from >= process->channels_size || from == PARENT_ID) {
         return -1;
     }
     Channel *channel = &process->channels[from];
@@ -116,6 +117,7 @@ int receive(void *self, local_id from, Message *msg) {
     if (channel_read(channel, msg) != 0) {
         return -1;
     }
+
     if (msg->s_header.s_magic != MESSAGE_MAGIC) {
         return -1;
     }
@@ -123,88 +125,81 @@ int receive(void *self, local_id from, Message *msg) {
 }
 
 int receive_any(void *self, Message *msg) {
-    return -1;
+    return 0;
 }
 
-static void init_message(Message* message, MessageType type, char* buffer, size_t buffer_size) {
-    *message = (Message) {
-            .s_header = (MessageHeader) {
-                    .s_magic = MESSAGE_MAGIC,
-                    .s_payload_len = buffer_size,
-                    .s_local_time = 0,
-                    .s_type = type
-            }
-    };
-    memcpy(message->s_payload, buffer, buffer_size);
-}
-
-static int child_handle(Process *self) {
+static void child_code_continue(Process *self) {
+    // started
     char str_buffer[1024];
-    size_t str_size;
-
-    // send started
-    str_size = sprintf(str_buffer, log_started_fmt, self->id, getpid(), getppid());
+    size_t str_size = sprintf(str_buffer, log_started_fmt, self->id, getpid(), getppid());
     printf(log_started_fmt, self->id, getpid(), getppid());
     fprintf(event_log_fd, log_started_fmt, self->id, getpid(), getppid());
     fflush(event_log_fd);
 
-    Message start_message;
-    init_message(&start_message, STARTED, str_buffer, str_size);
-    if (send_multicast(self, &start_message) != 0) {
-        perror("Child send multicast");
-        return -1;
-    }
+    Message start_message = (Message) {
+            .s_header = (MessageHeader) {
+                    .s_magic = MESSAGE_MAGIC,
+                    .s_payload_len = str_size,
+                    .s_local_time = 0,
+                    .s_type = STARTED
+            }
+    };
+    memcpy(start_message.s_payload, str_buffer, str_size);
+    send_multicast(self, &start_message);
 
     // wait all started
-    for (local_id i = FIRST_CHILD_ID; i < self->channels_size; i++) {
+    for (local_id i = 1; i < self->channels_size; i++) {
         if (i == self->id) {
             continue;
         }
         Message msg;
         if (receive(self, i, &msg) != 0 || msg.s_header.s_type != STARTED) {
             perror("receive");
-            return -1;
+            return;
         }
     }
     printf(log_received_all_started_fmt, self->id);
     fprintf(event_log_fd, log_received_all_started_fmt, self->id);
     fflush(event_log_fd);
 
-    // do work
+    // todo: work
 
-    // send finish
+    // finish
     str_size = sprintf(str_buffer, log_done_fmt, self->id);
     printf(log_done_fmt, self->id);
     fprintf(event_log_fd, log_done_fmt, self->id);
     fflush(event_log_fd);
 
-    Message finish_message;
-    init_message(&finish_message, DONE, str_buffer, str_size);
-    if (send_multicast(self, &finish_message) != 0) {
-        perror("Child multicast send");
-        return -1;
-    }
+    Message finish_message = (Message) {
+            .s_header = (MessageHeader) {
+                    .s_magic = MESSAGE_MAGIC,
+                    .s_payload_len = str_size,
+                    .s_local_time = 0,
+                    .s_type = DONE
+            }
+    };
+    memcpy(finish_message.s_payload, str_buffer, str_size);
+    send_multicast(self, &finish_message);
 
     // wait all done
-    for (local_id i = FIRST_CHILD_ID; i < self->channels_size; i++) {
+    for (local_id i = 1; i < self->channels_size; i++) {
         if (i == self->id) {
             continue;
         }
         Message msg;
         if (receive(self, i, &msg) != 0 || msg.s_header.s_type != DONE) {
             perror("receive");
-            return -1;
+            return;
         }
     }
     printf(log_received_all_done_fmt, self->id);
     fprintf(event_log_fd, log_received_all_done_fmt, self->id);
     fflush(event_log_fd);
-    return 0;
 }
 
-static void parent_handle(Process *self) {
+static void parent_code_continue(Process *self) {
     // wait all started
-    for (local_id i = FIRST_CHILD_ID; i < self->channels_size; i++) {
+    for (local_id i = 1; i < self->channels_size; i++) {
         if (i == self->id) {
             continue;
         }
@@ -216,7 +211,7 @@ static void parent_handle(Process *self) {
     }
 
     // wait all done
-    for (local_id i = FIRST_CHILD_ID; i < self->channels_size; i++) {
+    for (local_id i = 1; i < self->channels_size; i++) {
         if (i == self->id) {
             continue;
         }
@@ -244,17 +239,16 @@ static pipe_desc *open_pipes(size_t n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i == j) {
-                // Skip self-to-self pipes
                 matrix_set(matrix, n, i, j, (pipe_desc) {
-                        .data[0] = EMPTY_DESCRIPTOR,
-                        .data[1] = EMPTY_DESCRIPTOR
+                        .data[0] = -1,
+                        .data[1] = -1
                 });
                 continue;
             }
             fprintf(pipes_log_fd, "Opened pipe [%d -> %d]\n", i, j);
             fflush(pipes_log_fd);
-            if (pipe(matrix_get(matrix, n, i, j)->data) == EMPTY_DESCRIPTOR) {
-                perror("Failed to open pipe");
+            if (pipe(matrix_get(matrix, n, i, j)->data) == -1) {
+                perror("pipe");
                 exit(EXIT_FAILURE);
             }
         }
@@ -262,41 +256,37 @@ static pipe_desc *open_pipes(size_t n) {
     return matrix;
 }
 
-static Channel *collect_channels(pipe_desc *pipes_matrix, size_t n, size_t proc_id) {
+static Channel *extract_channels(pipe_desc *pipes_matrix, size_t n, size_t x) {
     Channel *channels = malloc(sizeof(Channel) * n);
     if (channels == NULL) {
         return NULL;
     }
-    for (size_t id = 0; id < n; id++) {
-        if (id == proc_id) {
-            // Skip self-to-self channel
-            channels[id] = (Channel) {
-                    .rfd = EMPTY_DESCRIPTOR,
-                    .wfd = EMPTY_DESCRIPTOR
+    for (size_t i = 0; i < n; i++) {
+        if (i == x) {
+            channels[i] = (Channel) {
+                    .rfd = -1,
+                    .wfd = -1
             };
             continue;
         }
-        pipe_desc *write_pipe = matrix_get(pipes_matrix, n, proc_id, id);
-        pipe_desc *read_pipe = matrix_get(pipes_matrix, n, id, proc_id);
-        channels[id] = (Channel) {
+        pipe_desc *write_pipe = matrix_get(pipes_matrix, n, x, i);
+        pipe_desc *read_pipe = matrix_get(pipes_matrix, n, i, x);
+        channels[i] = (Channel) {
                 .rfd = read_pipe->data[0],
                 .wfd = write_pipe->data[1]
         };
-        read_pipe->data[0] = EMPTY_DESCRIPTOR;
-        write_pipe->data[1] = EMPTY_DESCRIPTOR;
+        read_pipe->data[0] = -1;
+        write_pipe->data[1] = -1;
     }
     for (size_t i = 0; i < n * n; i++) {
         for (size_t j = 0; j < 2; j++) {
             int fd = pipes_matrix[i].data[j];
-            if (fd != EMPTY_DESCRIPTOR) {
-                fprintf(pipes_log_fd, "Closed file descriptor [%zu -> %zu]\n", i, j);
+            if (fd != -1) {
+                fprintf(pipes_log_fd, "Closed fd [%zu -> %zu]\n", i, j);
                 fflush(pipes_log_fd);
-                if (close(fd) != 0) {
-                    perror("Failed to close file descriptor");
-                    exit(EXIT_FAILURE);
-                }
+                close(fd);
             }
-            pipes_matrix[i].data[j] = EMPTY_DESCRIPTOR;
+            pipes_matrix[i].data[j] = -1;
         }
     }
     return channels;
@@ -305,115 +295,38 @@ static Channel *collect_channels(pipe_desc *pipes_matrix, size_t n, size_t proc_
 static void free_channels(Channel *channels, local_id channels_size) {
     for (local_id i = 0; i < channels_size; i++) {
         Channel *channel = &channels[i];
-        if (channel->rfd != EMPTY_DESCRIPTOR) {
-            fprintf(pipes_log_fd, "Closed read file descriptor [%d: %d]\n", current_id, i);
+        if (channel->rfd != -1) {
+            fprintf(pipes_log_fd, "Closed rfd [%d: %d]\n", current_pid, i);
             close(channel->rfd);
         }
-        if (channel->wfd != EMPTY_DESCRIPTOR) {
-            fprintf(pipes_log_fd, "Closed write file descriptor [%d: %d]\n", current_id, i);
+        if (channel->wfd != -1) {
+            fprintf(pipes_log_fd, "Closed wfd [%d: %d]\n", current_pid, i);
             close(channel->wfd);
         }
     }
     free(channels);
 }
 
-static int run_child_process(local_id id, local_id n, pipe_desc *matrix) {
-    pid_t pid = fork();
-    if (pid == -1) {
-        free(matrix);
-        perror("fork");
-        return -1;
-    }
-    if (pid > 0) {
-        return 0;
-    }
-
-    // child code
-    current_id = id;
-    Channel *channels = collect_channels(matrix, n, id);
-    free(matrix);
-    if (channels == NULL) {
-        perror("malloc");
-        return -1;
-    }
-
-    Process cps = (Process) {
-            .channels = channels,
-            .channels_size = n,
-            .id = id
-    };
-
-    if (child_handle(&cps) != 0) {
-        perror("Child process error");
-        return -1;
-    }
-
-    free_channels(channels, n);
-    fclose(pipes_log_fd);
-    fclose(event_log_fd);
-    return -1;
-}
-
-static void wait_all_child_processes(void) {
-    while (wait(NULL) > 0);
-}
-
-int run_processes(local_id n) {
-    pipe_desc *matrix = open_pipes(n);
-    if (matrix == NULL) {
-        perror("malloc");
-        return -1;
-    }
-
-    for (local_id i = 1; i < n; i++) {
-        if (run_child_process(i, n, matrix) != 0) {
-            return -1;
-        }
-    }
-
-    // parent code
-    current_id = PARENT_ID;
-    Channel *channels = collect_channels(matrix, n, PARENT_ID);
-    free(matrix);
-    if (channels == NULL) {
-        perror("malloc");
-        return -1;
-    }
-
-    Process parent_process = (Process) {
-            .id = PARENT_ID,
-            .channels = channels,
-            .channels_size = n,
-    };
-
-    parent_handle(&parent_process);
-    free_channels(channels, n);
-    wait_all_child_processes();
-    return 0;
-}
-
-struct {
-    local_id n;
-} arguments;
-
-int parse_arguments(int argc, char *argv[]) {
-    int opt;
-    while ((opt = getopt(argc, argv, "p:")) != -1) {
-        switch (opt) {
-            case 'p':
-                arguments.n = atoi(optarg);
-                break;
-            default:
-                fprintf(stderr, "Unknown option %c\n", opt);
-                return -1;
-        }
-    }
-    return 0;
-}
-
 int main(int argc, char *argv[]) {
-    if (parse_arguments(argc, argv) != 0) {
-        return -1;
+    char *process_str = NULL;
+    const char *short_options = "p:";
+    const struct option long_options[] = {
+            {"p", required_argument, NULL, 'p'},
+            {NULL, 0,                NULL, 0}
+    };
+    int rez;
+    int option_index;
+    while ((rez = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1) {
+        switch (rez) {
+            case 'p': {
+                process_str = optarg;
+                break;
+            }
+            default: {
+                fprintf(stderr, "Unknown option: %s", optarg);
+                return EXIT_FAILURE;
+            }
+        }
     }
 
     pipes_log_fd = fopen(pipes_log, "a");
@@ -426,10 +339,66 @@ int main(int argc, char *argv[]) {
         perror("fopen");
         exit(EXIT_FAILURE);
     }
+    current_pid = PARENT_ID;
 
-    local_id processes_count = arguments.n + 1;
-    run_processes(processes_count);
+    local_id x = atoi(process_str); // number of child processes
+    local_id n = x + 1; // number of processes
+    pipe_desc *matrix = open_pipes(n);
+    if (matrix == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
 
+    pid_t cpid;
+    for (local_id i = 1; i < n; i++) {
+        cpid = fork();
+        if (cpid == -1) {
+            free(matrix);
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        if (cpid == 0) {
+            // child code
+            current_pid = i;
+            Channel *channels = extract_channels(matrix, n, i);
+            free(matrix);
+            if (channels == NULL) {
+                perror("malloc");
+                exit(EXIT_FAILURE);
+            }
+
+            Process cps = (Process) {
+                    .channels = channels,
+                    .channels_size = n,
+                    .id = i
+            };
+
+            child_code_continue(&cps);
+
+            free_channels(channels, n);
+            fclose(pipes_log_fd);
+            fclose(event_log_fd);
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    // parent code
+    Channel *channels = extract_channels(matrix, n, 0);
+    free(matrix);
+    if (channels == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    parent_code_continue(&(Process) {
+            .id = 0,
+            .channels = channels,
+            .channels_size = n
+    });
+
+    free_channels(channels, n);
+
+    while (wait(NULL) > 0);
     fclose(pipes_log_fd);
     fclose(event_log_fd);
 }
